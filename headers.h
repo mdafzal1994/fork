91a8b4aca7d571d1165df2efb67815f0e85137b6
mdafzal1994 <mdafzal1994@gmail.com>
2020-10-15 11:22:35 +0530

    add first

---
+++ headers.h
@@ -0,0 +1,5 @@
+#include<stdio.h>
+
+void add(int,int);
+void sub(int,int);
+
---
+++ hello.c
@@ -0,0 +1,12 @@
+//#include<stdio.h>
+#include"headers.h"
+
+
+int main()
+{
+
+  add(3,6);
+  sub(9,4);
+  return 0;
+
+}
