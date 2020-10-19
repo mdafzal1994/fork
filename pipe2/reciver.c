0ae38ce31223b36f5af6737ded8eba5646bd7872
mdafzal1994 <mdafzal1994@gmail.com>
2020-10-19 20:50:07 +0530

    sender snd receiver pipe unrelated

--- pipe2/reciver.c
+++ pipe2/reciver.c
@@ -10,7 +10,7 @@
 int main()
 {
    int fd;
-   char rbuffer[128];
+   char rbuffer;
   fd=open("desd",O_RDONLY);
   if(fd==-1)
   {
@@ -20,8 +20,30 @@ int main()
   
   } 
  
-  read(fd,rbuffer,128);
-  printf("receive : %s\n",rbuffer);
+  //read(fd,rbuffer,128);i
+  int sum=0;
+  while (read(fd,&rbuffer, 1) > 0)
+  {      
+         sum+=rbuffer-'0';
+ 	 //printf("receive : %c\n",rbuffer);
+  }
+  //printf("sum sended : %d\n",sum);
+
+  close(fd);
+
+
+//send after adding  number 
+fd=open("desd",O_WRONLY);
+  if(fd==-1)
+  {
+
+    perror("file not found\n");
+    exit(0);
+
+  }
+   char c[10];
+   c[0]=sum+'0';
+  write(fd,c,1);
   close(fd);
 
 
--- pipe2/sender.c
+++ pipe2/sender.c
@@ -4,13 +4,14 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include<string.h>
 
 //sender
 
 int main()
 {
    int fd;
-  fd=open("desd",O_WRONLY);
+/*  fd=open("desd",O_WRONLY);
   if(fd==-1)
   {
   
@@ -18,10 +19,56 @@ int main()
     exit(0);
   
   } 
- 
-  write(fd,"CDAC\n",5);
-  close(fd);
+ */
+  int t=2;
+  char c[10];
+  int n;
+  int i=0;
+while(t--)
+{
+	scanf("%d",&n);
+	c[i++]=n+'0';
+	printf("data enter %c \n",c[i-1]);
+}
+
+fd=open("desd",O_WRONLY);
+  if(fd==-1)
+  {
+
+    perror("file not found\n");
+    exit(0);
+
+  }
+
+
+  write(fd,c,2);//strlen(n));
+  
   printf("data send\n");
+  close(fd);
+// read from sender 
+
+  char rbuffer;
+  fd=open("desd",O_RDONLY);
+  if(fd==-1)
+  {
+
+    perror("read file not found\n");
+    exit(0);
+
+  }
+
+  //read(fd,rbuffer,128);i
+  int sum=0;
+  while (read(fd,&rbuffer, 1) > 0)
+  {
+         
+         printf("receive sum : %c\n",rbuffer);
+  }
+  
+
+  close(fd);
+
+
 
 
 return 0;
