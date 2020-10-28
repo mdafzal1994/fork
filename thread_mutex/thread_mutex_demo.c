fcc9101777191c091a34abb66820f298e3321445
mdafzal1994 <mdafzal1994@gmail.com>
2020-10-28 23:35:18 +0530

    recusion mutex vx non recursion

--- thread_mutex/thread_mutex_demo.c
+++ thread_mutex/thread_mutex_demo.c
@@ -2,19 +2,23 @@
 #include<stdio.h>
 #include <pthread.h>
 
-/*       int pthread_create(pthread_t *thread, const pthread_attr_t *attr,
-                          void *(*start_routine) (void *), void *arg);
-*/
+/*  avoid RACE condition     done  by using  locking and unlocking   */
+
+
+pthread_mutex_t  mutex_count;   // create mutex
+
+
 
 int count=0;
 void *thread_inc(void *arg)
 {
    while(1)
    {
-  	count++;
-   
-	printf("inc thread %d \n",count);
+	        pthread_mutex_lock(&mutex_count);
+  		count++;
+   		printf("inc thread %d \n",count);
 
+	        pthread_mutex_unlock(&mutex_count);
    }
 }
 
@@ -24,9 +28,12 @@ void *thread_dec(void *arg)
  while(1)
  {
 
+	        pthread_mutex_lock (&mutex_count);
 	count--;
 
 	printf("dec thread %d \n",count);
+
+	        pthread_mutex_unlock (&mutex_count);
  }
 }
 
@@ -34,12 +41,17 @@ void *thread_dec(void *arg)
 {
         pthread_t tid_inc;// store id of thread
 	pthread_t tid_dec;
+      /*initializing mutex    return typ  0 -> suc    -1 -> unscs */
+       	pthread_mutex_init (&mutex_count,NULL);     //NULL atrrb means    take mutex as non recursive
+
         pthread_create(&tid_inc,NULL,thread_inc,NULL);
 
         pthread_create(&tid_dec,NULL,thread_dec,NULL);
      pthread_join(tid_inc,NULL);
 
      pthread_join(tid_dec,NULL);
+     
+     pthread_mutex_destroy (&mutex_count);
    return 0;
 
 
